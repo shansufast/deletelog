93164dd1643a89a30429e7372a87820c98f1642f
sugertanyong <sugertanyong@gmail.com>
2020-01-14 04:03:44 +0800

    Update rmlog.sh
    7天删除error.log 30天删除access.log

--- rmlog.sh
+++ rmlog.sh
@@ -1,1 +1,2 @@
-find /var/log/v2ray/error.log -mtime +7 -name “*.*” -exec rm -rf {} \;
+find /var/log/v2ray/error.log -mtime +7 -name “error.log” -exec rm -rf {} \;
+find /var/log/v2ray/error.log -mtime +30 -name “access.log” -exec rm -rf {} \;
